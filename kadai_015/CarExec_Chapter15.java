1c4eea1e8b39d5fbcb485f884f0de34e73b8eabf
kya-yuri <kttnnimnwnksuk@gmail.com>
2024-09-22 17:26:23 +0900

    課題提出_20240922

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,11 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		Car_Chapter15 gearChange1 = new Car_Chapter15();
+		gearChange1.gearChange(3);
+		gearChange1.run();
+	}
+
+}
---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,26 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+	//フィールド初期化
+	private int gear = 1;
+	private int speed = 10;
+	
+	//ギアの値により速度を変える
+	public void gearChange(int afterGear) {
+		gear = afterGear;
+		switch(gear) {
+			case 1 -> speed = 10;
+			case 2 -> speed = 20;
+			case 3 -> speed = 30;
+			case 4 -> speed = 40;
+			case 5 -> speed = 50;
+			default -> speed = 10;
+		}
+	}
+	
+	//ギアチェンジ後の速度を表示する
+	public void run() {
+		System.out.println("ギア1から" + gear + "に切り替えました");
+		System.out.println("速度は時速" + speed + "kmです");
+	}
+}
