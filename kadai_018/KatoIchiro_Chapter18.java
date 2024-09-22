17a7f49b1d04753803a24b6a83cdc9222f89973d
kya-yuri <kttnnimnwnksuk@gmail.com>
2024-09-22 23:58:34 +0900

    課題提出_20240922

---
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -0,0 +1,15 @@
+package kadai_018;
+
+public class KatoIchiro_Chapter18 extends Kato_Chapter18 {
+	//名を表すフィールドの値をセットする
+	public void setGivenName() {
+		givenName = "一郎";
+	}
+	
+	//eachIntroduceの実装
+	public void eachIntroduce() {
+		System.out.println("好きな食べ物はリンゴです");
+		System.out.println("");
+	}
+	
+}
---
+++ kadai_018/KatoTaro_Chapter18.java
@@ -0,0 +1,15 @@
+package kadai_018;
+
+public class KatoTaro_Chapter18 extends Kato_Chapter18 {
+	//名を表すフィールドの値をセットする
+	public void setGivenName() {
+		givenName = "太郎";
+	}
+	
+	//eachIntroduceの実装
+	public void eachIntroduce() {
+		System.out.println("Javaが得意です");
+		System.out.println("");
+	}
+	
+}
